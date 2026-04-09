a9d8831b5fc16b27ac52fb4d5b7cabf1676cf48c
Luis Henrique Araujo <lui.hh.araujo@gmail.com>
2026-04-09 20:35:31 -0300

    'fim'

--- routes/api.php
+++ routes/api.php
@@ -4,23 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
-use App\Http\Controllers\Api\SeederController;
-
-use Illuminate\Support\Facades\Artisan;
-
-Route::get('/run-seeder', function () {
-    Artisan::call('db:seed');
-
-    return response()->json([
-        'message' => 'Seeder executado com sucesso!'
-    ]);
-});
-
-// Rotas de seed via API
-Route::post('/seed/users', [SeederController::class, 'seedUsers']);
-Route::post('/seed/categories', [SeederController::class, 'seedCategories']);
-Route::post('/seed/posts', [SeederController::class, 'seedPosts']);
-Route::post('/seed/all', [SeederController::class, 'seedAll']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
